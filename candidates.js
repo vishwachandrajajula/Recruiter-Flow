ed56af8471bd525625b7845702245bf93f473478
vishwachandrajajula <159527419+vishwachandrajajula@users.noreply.github.com>
2025-12-25 21:02:07 +0530

    Add files via upload

---
+++ candidates.js
@@ -0,0 +1,6 @@
+// src/data/candidates.js
+export const candidates = [
+  { id: 1, name: "Alice Johnson", role: "Frontend Developer" },
+  { id: 2, name: "Bob Smith", role: "Backend Developer" },
+  { id: 3, name: "Charlie Brown", role: "Data Analyst" },
+];
